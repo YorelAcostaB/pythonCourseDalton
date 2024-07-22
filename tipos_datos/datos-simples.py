372a2062073ff0f9db79a8a31a91ad64c1214a6b
Yorel Acosta <yorelacostabergonzoni@gmail.com>
2024-07-22 09:50:48 -0300

    Clase 1

---
+++ tipos_datos/datos-simples.py
@@ -0,0 +1,17 @@
+"string"
+'string'
+
+"""tus datos son:
+        nombre: Yorel
+        apellido: Acosta"""
+
+'''tus datos son:
+        nombre: Yorel
+        apellido: Acosta'''
+
+
+4 # Tipo de datos: número entero: int
+
+4.4 # Tipo de datos: número punto flotante: floant
+
+True # Tipo de datos: boolean
---
+++ variables/variable.py
@@ -0,0 +1,24 @@
+# Definir varialbe con camelCase
+nombreCompletoDeLaPersona = 'Yorel Acosta'
+
+# Definir variable con snake_case
+nombre_completo_de_la_persona = 'Yorel Acosta'
+
+# Definiendo una variable
+nombre = True
+
+# Eliminando una variable de la mem
+#del nombre
+
+# Concatenar con f-strings
+bienvenida = f'Hola {nombre} ¿Cómo estás?' 
+
+# Concatenar con +
+bienvenida = 'Hola ' + '¿Cómo estás?' 
+
+# Imprimir datos
+print(bienvenida)
+
+# Operadores de pertenenia (in / not in)
+print('pedro' in bienvenida) # True
+print('pedro' not in bienvenida) # False
